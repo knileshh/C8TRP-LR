aadf6e0443770e6635e513240b6773577574486d
knileshh <neutron_stays@protonmail.com>
2024-08-09 22:53:21 +0530

    Done with basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { StrictMode } from 'react'; 
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import Home from './Home.jsx';
+import TextToSpeech from './TextToSpeech.jsx';
+import VoiceToText from './VoiceToText.jsx';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vt" element={<VoiceToText />} />
+        <Route path="/ts" element={<TextToSpeech />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
