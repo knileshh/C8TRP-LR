582cc3ce209c3ef022b08b31a17d7bdb2d4544c2
knileshh <neutron_stays@protonmail.com>
2024-08-10 07:29:30 +0530

    Add .env and be test

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the dark side');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
